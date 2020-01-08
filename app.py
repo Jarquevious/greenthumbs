4e7808d92b97e1bd7deae8743c1da6803cf026c4
Jarquevious <jarquevious@gmail.com>
2020-01-07 16:32:31 -0800

    second commit

---
+++ app.py
@@ -0,0 +1,79 @@
+"""Greenthumbs bookings site"""
+from flask import Flask, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
+import os
+
+
+# Import pymongo and initialize client
+from pymongo import MongoClient
+
+
+host = os.environ.get('MONGODB_URI','mongodb://localhost:27017/GreenThumps')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+appointments = db.appointments
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Show all Appointments."""
+    return render_template('home.html', appointments=appointments.find())
+
+@app.route('/book_appointment')
+def show_book_appointment_form():
+    """Show Book Appointment Form"""
+    return render_template('book_appointment.html')
+
+@app.route('/book_appointment', methods=['Post'])
+def submit_appointment():
+    """Submit new appointment."""
+    print(request.form)
+    appointment = {
+       'firstname':request.form.get('firstname'),
+       'lastname':request.form.get('lastname'),
+
+   }
+    appointment_id = appointments.insert_one(appointment).inserted_id
+    return redirect(url_for('appointment_show', appointment_id=appointment_id))
+
+@app.route('/appointments/<appointment_id>', methods=['GET'])
+def appointment_show(appointment_id):
+    """Show a single appointment."""
+    appointment = appointments.find_one({'_id': ObjectId(appointment_id)})
+    return render_template('appointment_show.html', appointment=appointment)
+
+@app.route('/appointment/<appointment_id>/edit')
+def appointment_edit(appointment_id):
+    """Show the edit form for an appointment."""
+    appointment = appointments.find_one({'_id': ObjectId(appointment_id)})
+    return render_template('appointment_edit.html', appointment=appointment, title='Edit Appointment')
+
+@app.route('/appointment/<appointment_id>', methods=['POST'])
+def appointment_update(appointment_id):
+    """Submit an edited appointment."""
+    updated_appointment = {
+        'firstname':request.form.get('firstname'),
+        'lastname':request.form.get('lastname'),
+        'address':request.form.get('address'),
+        'city':request.form.get('city'),
+        'state':request.form.get('state'),
+        'zipcode':request.form.get('zipcode'),
+        'bday':request.form.get('bday'),
+        'service':request.form.get('service') 
+       }
+
+    appointments.update_one({'_id': ObjectId(appointment_id)}, {'$set': updated_appointment})
+    return redirect(url_for('appointment_show', appointment_id=appointment_id))
+
+
+@app.route('/appointment/<appointment_id>/delete', methods=['POST'])
+def appointment_delete(appointment_id):
+    """Delete one appointment."""
+    appointments.delete_one({'_id': ObjectId(appointment_id)})
+    return redirect(url_for('index'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
